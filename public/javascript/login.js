44c12da1a50a1215d2321c515b215046b6968829
Evelyn Frech <evelynmariafrech@gmail.com>
2022-05-07 23:56:40 -0400

    create login and signup code

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,22 +1,25 @@
-const loginFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-login');
-    const passwordEl = document.querySelector('#password-input-login');
-  
-    const response = await fetch('/api/user/login', {
-          // Create the functionality to help create the buttons for your website.
+const loginFormHandler = async function (event) {
+  event.preventDefault();
 
+  const username = document.querySelector('#username-input-login').value.trim();
+  const password = document.querySelector('#password-input-login').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users/login', {
+      // Create the functionality to help create the buttons for your website.
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
-  
+
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
       alert('Failed to login');
     }
-  };
-  
-  document
-    .querySelector('#login-form')
-    .addEventListener('submit', loginFormHandler);
-  
+  }
+};
+
+document
+  .querySelector('.login-form')
+  .addEventListener('submit', loginFormHandler);
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,21 +1,25 @@
-const signupFormHandler = async function(event) {
-    event.preventDefault();
-  
-    const usernameEl = document.querySelector('#username-input-signup');
-    const passwordEl = document.querySelector('#password-input-signup');
-  
-    const response = await fetch('/api/user', {
-         // Create the functionality to help create the buttons for your website.
+const signupFormHandler = async function (event) {
+  event.preventDefault();
 
+  const username = document.querySelector('#username-input-signup').value.trim();
+  const password = document.querySelector('#password-input-signup').value.trim();
+
+  if (username && password) {
+    const response = await fetch('/api/users', {
+      // Create the functionality to help create the buttons for your website.
+      method: 'POST',
+      body: JSON.stringify({ username, password }),
+      headers: { 'Content-Type': 'application/json' },
     });
-  
+
     if (response.ok) {
       document.location.replace('/dashboard');
     } else {
       alert('Failed to sign up');
     }
-  };
-  
-  document
-    .querySelector('#signup-form')
-    .addEventListener('submit', signupFormHandler);
+  }
+};
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
